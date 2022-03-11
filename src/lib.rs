8e98bccb75a16d20939d7c41ba68c0510482dcfa
Taennan Rickman <Taennan@iMac.lan>
2022-03-12 08:02:22 +1200

    added set() method

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,155 @@
+/*!
+
+A simple multidimensional point struct, based on an array.
+
+See the ```PointND``` struct for basic usage
+
+ */
+
 use std::ops::{Deref, DerefMut};
 
+
+/**
+
+The whole _point_ of the crate (get it?)
+
+This is really just a wrapper around an array with convenience methods for accessing values if it's dimensions are within ```1..=4```
+
+# Examples
+
+## Constructing a Point
+
+No matter how a PointND is constructed, the second generic arg must be filled with the number of dimensions it needs to have (i.e - the length of the array to push on the stack)
+
+If a point of zero dimensions is constructed, it will panic
+
+```
+use point_nd::PointND;
+
+// Creates a 2D point from values of a given vector or array
+let vec: Vec<i32> = vec![0, 1];
+let p: PointND<_, 2> = PointND::from(&vec);
+
+// Creates a 3D point with all values set to 5
+//  When using this function, complete type annotation is necessary
+let p: PointND<i32, 3> = PointND::fill(5);
+
+// ERROR: Can't create a point with zero dimensions
+// let p: PointND<_, 0> = PointND::fill(9);
+
+// If you don't like writing PointND twice, use this syntax instead
+//  Note: The second generic must still be specified
+let p = PointND::<_, 2>::from(&vec);
+```
+
+## Accessing Values
+
+It is recommended to use the convenience getters if the dimensions of the point are from ```1..=4```
+
+```
+use point_nd::PointND;
+
+// A 2D point
+let arr: [i32; 2] = [0,1];
+let p: PointND<_, 2> = PointND::from(&arr);
+
+// As the point has 2 dimensions, we can access it's values with the x() and y() methods
+let x: &i32 = p.x();
+let y = p.y();
+
+assert_eq!(*y, arr[1]);
+
+// If the point had 3 dimensions, we could use the above and:
+// let z = p.z();
+
+// Or 4:
+// ...
+// let w = p.w();
+```
+
+Otherwise indexing or the ```get()``` method can be used
+
+```
+use point_nd::PointND;
+
+let arr: [i32; 2] = [0,1];
+let p: PointND<_, 2> = PointND::from(&arr);
+
+// Safely getting
+//  Returns None if index is out of bounds
+let x: Option<&i32> = p.get(0);
+assert_eq!(*x.unwrap(), arr[0]);
+
+// Unsafely indexing
+//  If the index is out of bounds, this will panic
+//  Note that unlike other accessing methods, this will return a copy of the value
+let y: i32 = p[1];
+assert_eq!(y, arr[1]);
+```
+
+## Querying Size
+
+The number of dimensions can be retrieved using the ```dims()``` method (short for _dimensions_)
+
+```
+use point_nd::PointND;
+
+let p: PointND<i32, 2> = PointND::fill(10);
+assert_eq!(p.dims(), 2);
+```
+ */
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct PointND<T, const N: usize>([T; N])
     where T: Clone + Copy + Default;
 
+
 impl<T, const N: usize> PointND<T, N>
     where T: Clone + Copy + Default {
 
+    /**
+     Returns a new ```PointND``` with values from the specified array or vector
+
+     ### Panics
+
+     If the length of the slice is zero
+     */
     pub fn from(slice: &[T]) -> Self {
         if slice.len() == 0 {
-            panic!("Cannot construct Point with zero dimensions");
+            panic!("Cannot construct PointND with zero dimensions");
         }
         let arr: [T; N] = slice.try_into().unwrap();
         PointND(arr)
     }
 
+    /**
+     Returns a new ```PointND``` with all values set as specified
+
+     ### Panics
+
+     If the dimensions of the point being constructed is zero
+     */
     pub fn fill(value: T) -> Self {
         PointND::<T, N>::from(&[value; N])
     }
 
 
+    /**
+     Returns the number of dimensions of the point (a 2D point will return 2, a 3D point 3, _etc_)
+
+     Equivalent to calling ```len()```
+     */
     pub fn dims(&self) -> usize {
         self.len()
+    }
+
+
+    /**
+     */
+    pub fn set(&mut self, i: usize, new_val: T) -> Result<(), ()> {
+        if self.dims() < i { return Err(()) }
+
+        self[i] = new_val;
+        Ok(())
     }
 
 
@@ -78,6 +206,7 @@ impl<T, const N: usize> PointND<T, N>
     }
 
 }
+
 
 impl<T, const N: usize> Deref for PointND<T, N>
     where T: Clone + Copy + Default {
